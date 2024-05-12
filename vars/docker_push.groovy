b660f16e1a80f21f43e7bc48f21fedcfc42ae83a
Madhup Pandey <121779953+DevMadhup@users.noreply.github.com>
2024-05-12 13:39:14 +0530

    Create docker_push.groovy

---
+++ vars/docker_push.groovy
@@ -0,0 +1,5 @@
+def call(String ){
+  withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
+      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+  }
+}
