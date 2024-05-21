2dd1e459afbf4b8f2e98e3412d8a045dcd13c8de
Madhup Pandey <121779953+DevMadhup@users.noreply.github.com>
2024-05-21 21:41:14 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
 // Define function
-def call(String ProjectName, String ImageTag, String DockerHubUser){
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+def call(String ProjectName, String ImageTag, String DockerHubUser, String Path){
+  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} -f $Path/Dockerfile"
 }
